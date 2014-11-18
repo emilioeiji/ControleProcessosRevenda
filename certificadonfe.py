3d54c51797db9a7ae333c90d6b7be455b0879eea
Emilio Eiji <contato@emilioeiji.com.br>
2014-11-17 22:43:44 -0200

    Iniciando as comparações de datas.

--- certificadonfe.py
+++ certificadonfe.py
@@ -1,2 +1,10 @@
 #/usr/bin/python3
 # -*- encoding: utf-8 -*-
+
+from datetime import datetime
+
+vencimento = datetime(2015, 6, 16)
+hoje = datetime.now()
+dias_vencimento = vencimento - hoje
+
+print(dias_vencimento.days)
